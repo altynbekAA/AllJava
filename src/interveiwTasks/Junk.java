7d67064cbac028438d2a7f60d012ab7d3b8073a4
altynbekabdurasulov <altynbekabdurasulov@172.16.12.2>
2018-05-28 11:55:53 -0500

    last version

--- src/interveiwTasks/Junk.java
+++ src/interveiwTasks/Junk.java
@@ -1,9 +1,13 @@
 package interveiwTasks;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
-public class UniqueNums {
+public class Junk {
 	public static void main(String[] args) {
 		int[] nums = new int[6];
 		nums[0] = 50;
@@ -14,20 +18,40 @@ public class UniqueNums {
 		nums[5] = 7;
 		Set<Integer> set = new HashSet<>();
 		for (int i = 0; i < nums.length; i++) {
-			set.add(nums[i]);
+		set.add(nums[i]);
 
 		}
-		System.out.println(set);							//1
-
+		System.out.println(set); //1
+		
+		
 		String str = "dawdawdasdwdwdwd";
 		Set<Character> set1 = new HashSet<>();
 		for (int i = 0; i < str.length(); i++) {
-			set1.add(str.charAt(i));
+		set1.add(str.charAt(i));
 		}
-        System.out.println(set1);								//2
+		System.out.println(set1); //2
+		
+	
+	
+	List list = new ArrayList<>();
+	for(int k = 0; k<str.length(); k++) {
+		list.add(str.charAt(k));
+	}
+	System.out.println(list);
+
+
+	
+	
+	
+	}
 	}
 
-}
+
+
+
+
+
+
 
 
 
